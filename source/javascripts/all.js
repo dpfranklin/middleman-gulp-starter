f28687d666a44b98d498ca4cf28fd744ff7b4f88
David Franklin <david@dpfranklin.com>
2016-05-09 18:07:39 -0400

    consolidate

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,17 +1,19 @@
-// This is where it all goes :)
 
+// expose jsquery to global environmemnt
 require("expose?$!jquery");
 
-var _    = require("lodash");
-var team = require('_test.js');
-var uniqt   = _.uniq(team);
-var take3t  = _.take(uniqt, 3)
+// test lodash
+var _      = require("lodash");
+var team   = require('_test.js');
+var uniqt  = _.uniq(team);
+var take3t = _.take(uniqt, 3)
 
 console.log('full team list:  ' + team);
 console.log('Lodash removed redundancies:  ' + uniqt);
 console.log('Lodash took 3:  ' + take3t);
 console.log( "Exposed jQuery version: " + $.fn.jquery );
 
+// test jQuery availability on document ready
 $(function() {
   var jqtest = $("p.doc a").attr('href');
   console.log( 'Exposed jQuery found link url upon document ready: ' +jqtest );
